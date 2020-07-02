9d97bcb976b9b030a903cad1b6547b3a4f352e07
skorobahatko <maxim.skorobagatko@gmail.com>
2020-07-02 14:20:50 +0300

    added post, jsonplaceholder

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app/components/app/app.component';
 import { UserComponent } from './app/components/user/user.component';
 import {HttpClientModule} from '@angular/common/http';
+import { PostComponent } from './app/components/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [PostComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import {User} from '../../models/User';
 import {HttpClient} from '@angular/common/http';
+import {Post} from '../../models/Post';
 
 @Component({
   selector: 'app-root',
-  template: '<app-user *ngFor="let user of users" [userX]="user"></app-user>',
+  template: `<app-user *ngFor="let user of users" [userX]="user"></app-user>
+            <app-post *ngFor="let post of posts" [post]="post"></app-post>`,
   styles: ['']
 })
 export class AppComponent {
   title = 'ngDemo0';
 
   users: User[];
+  posts: Post[];
   constructor(private httpClient: HttpClient) {
     this.httpClient
       .get('https://jsonplaceholder.typicode.com/users')
       .subscribe(data => this.users = data);
+    this.httpClient
+      .get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+      .subscribe(data => this.posts = data);
   }
 }
